6b358e50f464523a8f787a9f8e392b368ddcd068
sara <sara@saradeMacBook-Pro.local>
2018-07-08 19:20:32 +0800

    add login/register

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 
@@ -14,8 +15,14 @@ bootstrap = Bootstrap(app)
 mail = Mail(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 from .main import main as main_blueprint
+from .auth import auth as auth_blueprint
 app.register_blueprint(main_blueprint)
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 
